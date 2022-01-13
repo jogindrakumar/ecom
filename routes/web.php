2bc2a331298790318bc2aab298b8369f0561a015
justcodingman <joginder9868@outlook.com>
2022-01-13 17:27:11 +0530

    modified:   app/Http/Controllers/AdminController.php 	new file:   app/Http/Controllers/Backend/AdminProfileController.php 	new file:   public/upload/no_image.jpg 	new file:   resources/views/admin/admin_profile_view.blade.php 	modified:   resources/views/admin/body/header.blade.php 	modified:   resources/views/admin/body/sidebar.blade.php 	modified:   resources/views/admin/index.blade.php 	new file:   resources/views/auth/admin_login.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\AdminProfileController;
 use App\Models\Admin;
 
 /*
@@ -31,6 +32,11 @@ Route::group(['prefix' => 'admin', 'middleware'=>['admin:admin']],function(){
     Route::post('/login',[AdminController::class,'store'])->name('admin.login');
 });
 
+
+//Admin All routes
+
+ Route::get('/admin/logout',[AdminController::class,'destroy'])->name('admin.logout');
+ Route::get('/admin/admin',[AdminProfileController::class,'AdminProfile'])->name('admin.profile');
 
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
     return view('admin.index');
