d17a37010877f36b72a3a49da5b5c8d14d393212
justcodingman <joginder9868@outlook.com>
2022-01-22 14:57:45 +0530

    modified:   resources/views/auth/reset-password.blade.php

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -24,20 +24,21 @@
 	
 	  <form method="POST" action="{{ route('password.update') }}">
             @csrf
+			<input type="hidden" name="token" value="{{ $request->route('token') }}">
 		<div class="form-group">
 		    <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
 		    <input type="email" name="email" class="form-control unicase-form-control text-input"  id="email" >
 		</div>
 	  	<div class="form-group">
 		    <label class="info-title" for="exampleInputEmail1">Password <span>*</span></label>
-		    <input type="password" name="password"  class="form-control unicase-form-control text-input" >
+		    <input type="password" name="password"  class="form-control unicase-form-control text-input" id="password">
 		</div>
         <div class="form-group">
 		    <label class="info-title" for="exampleInputEmail1">Confirm Password <span>*</span></label>
-		    <input type="email" name="email" class="form-control unicase-form-control text-input"  id="email" >
+		    <input type="password" name="password_confirmation" class="form-control unicase-form-control text-input" id="password_confirmation" >
 		</div>
 		
-	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Email Password Reset Link</button>
+	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Reset Password</button>
 	</form>					
 </div>
 <!-- Sign-in -->
