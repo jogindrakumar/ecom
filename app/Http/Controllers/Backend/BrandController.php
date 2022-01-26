78667fb135d0eb8b40aa5924cf40e406893ce84a
justcodingman <joginder9868@outlook.com>
2022-01-26 09:24:21 +0530

    modified:   app/Http/Controllers/Backend/BrandController.php 	modified:   composer.json 	modified:   composer.lock 	modified:   config/app.php 	new file:   config/image.php 	new file:   public/upload/brand/1722987403131044.png 	new file:   public/upload/brand/1722987606723363.png 	new file:   public/upload/brand/1722987638533697.png 	new file:   public/upload/brand/1722987693820944.png 	new file:   public/upload/brand/1722987732607826.png 	new file:   public/upload/brand/1722987774063180.png 	modified:   resources/views/backend/brand_view.blade.php

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Brand;
+use Image;
 class BrandController extends Controller
 {
     //
@@ -15,6 +16,28 @@ class BrandController extends Controller
     }
 
     public function BrandStore(Request $request){
-        
+        $request->validate([
+            'brand_name_en' => 'required',
+            'brand_name_hin' => 'required',
+            'brand_image' => 'required',
+        ]);
+
+        $image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+        $save_url = 'upload/brand/'.$name_gen;
+
+        Brand::insert([
+            'brand_name_en' => $request->brand_name_en,
+            'brand_name_hin' => $request->brand_name_hin,
+            'brand_slug_en' => strtolower(str_replace(' ','-',$request->brand_name_en)),
+            'brand_slug_hin' => strtolower(str_replace(' ','-',$request->brand_name_hin)),
+            'brand_image' => $save_url,
+        ]);
+         $notification = array(
+            'message' => 'Brand Inserted Successfully',
+            'alert-type' => 'success'
+                );
+        return redirect()->back()->with($notification);
     }
 }
--- resources/views/backend/brand_view.blade.php
+++ resources/views/backend/brand_view.blade.php
@@ -37,7 +37,7 @@
         <tr>
             <td>{{$item->brand_name_en}}</td>
             <td>{{$item->brand_name_hin}}</td>
-            <td><img src="{{asset($item->brand_image)}}" alt="" style="width:7px; height:40px;"></td>
+            <td><img src="{{asset($item->brand_image)}}" alt="" style="width:70px; height:40px;"></td>
             <td>
                 <a href="" class="btn btn-warning">Edit</a>
                 <a href="" class="btn btn-danger">Delete</a>
@@ -75,24 +75,36 @@
         <div class="form-group">
         <h5>Brand Name English<span class="text-danger">*</span></h5>
         <div class="controls">
-        <input type="text" name="brand_name_en" class="form-control"  value=""  > </div>
+        <input type="text" name="brand_name_en" class="form-control"  value=""  > 
+		@error('brand_name_en')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+		</div>
     </div>
     <div class="form-group">
         <h5>Brand Name Hindi<span class="text-danger">*</span></h5>
         <div class="controls">
-        <input type="text" name="brand_name_hin" class="form-control"  value=""  > </div>
+        <input type="text" name="brand_name_hin" class="form-control"  value=""  >
+		@error('brand_name_hin')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	 </div>
     </div>
     <div class="form-group">
         <h5>Brand Image<span class="text-danger">*</span></h5>
         <div class="controls">
-        <input type="file" name="brand_image"  class="form-control"  value=""  > </div>
+        <input type="file" name="brand_image"  class="form-control"  value=""  > 
+	@error('brand_image')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	</div>
     </div>
    
 
              
    
     <div class="text-xs-right">
-        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
+        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add New">
        
     </div>
 </form>
