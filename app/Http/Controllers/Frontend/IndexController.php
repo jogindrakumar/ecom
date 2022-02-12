b0130c62367f0d62d71ccf3392eb14038e38aee6
justcodingman <joginder9868@outlook.com>
2022-02-12 11:18:55 +0530

    modified:   app/Http/Controllers/Frontend/IndexController.php 	modified:   app/Models/MultiImg.php 	modified:   resources/views/frontend/product/product_details.blade.php

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Category;
 use App\Models\Slider;
 use App\Models\Product;
+use App\Models\MultiImg;
 use Illuminate\Support\Facades\Hash;
 class IndexController extends Controller
 {
@@ -96,6 +97,7 @@ class IndexController extends Controller
     // product details function 
      public function ProductDetails($id,$slug){
          $product = Product::findOrFail($id);
-         return view('frontend.product.product_details',compact('product'));
+         $multiImag = MultiImg::where('product_id',$id)->get();
+         return view('frontend.product.product_details',compact('product','multiImag'));
      }
 }
