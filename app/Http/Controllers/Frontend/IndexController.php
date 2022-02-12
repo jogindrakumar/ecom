dac79cebce89f9e8f9a8a300aa3ec0090f00bdff
justcodingman <joginder9868@outlook.com>
2022-02-12 11:45:59 +0530

    modified:   app/Http/Controllers/Frontend/IndexController.php 	modified:   resources/views/frontend/index.blade.php

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -19,7 +19,8 @@ class IndexController extends Controller
         $categories = Category::orderBy('category_name_en','ASC')->get();
         $products = Product::where('status',1)->orderBy('id','DESC')->get();
         $sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
-       return view('frontend.index',compact('categories','sliders','products'));
+        $featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
+       return view('frontend.index',compact('categories','sliders','products','featured'));
     }
 
     // user logout 
