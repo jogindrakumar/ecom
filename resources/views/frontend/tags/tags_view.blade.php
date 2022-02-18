830bee631318d8ac42cd99a1abaf744cae4335a7
justcodingman <joginder9868@outlook.com>
2022-02-18 12:07:25 +0530

    modified:   resources/views/frontend/tags/tags_view.blade.php

--- resources/views/frontend/tags/tags_view.blade.php
+++ resources/views/frontend/tags/tags_view.blade.php
@@ -41,29 +41,33 @@ Tag Wish Product
               <div class="sidebar-widget-body">
                 <div class="accordion">
                     @foreach($categories as $category)
-                  <div class="accordion-group">
-                   <div class="accordion-heading"> <a href="#collapse{{ $category->id }}" data-toggle="collapse" class="accordion-toggle collapsed"> 
+	<div class="accordion-group">
+	<div class="accordion-heading"> <a href="#collapse{{ $category->id }}" data-toggle="collapse" class="accordion-toggle collapsed"> 
 		@if(session()->get('language') == 'hindi') {{ $category->category_name_hin }} @else {{ $category->category_name_en }} @endif </a> </div>
-                    <!-- /.accordion-heading -->
-                   	<div class="accordion-body collapse" id="collapse{{ $category->id }}" style="height: 0px;">
-                      <div class="accordion-inner">
-    @php
+	<!-- /.accordion-heading -->
+	<div class="accordion-body collapse" id="collapse{{ $category->id }}" style="height: 0px;">
+	  <div class="accordion-inner">
+
+ @php
   $subcategories = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subcategory_name_en','ASC')->get();
   @endphp 
- @foreach($subcategories as $subcategory)
-                        <ul>
-@if(session()->get('language') == 'hindi') {{ $subcategory->subcategory_name_hin }} @else {{ $subcategory->subcategory_name_en }} @endif</a></li>
-                        </ul>
-                        	@endforeach 
 
-                            {{-- end sub category --}}
-                      </div>
-                      <!-- /.accordion-inner --> 
-                    </div>
-                    <!-- /.accordion-body --> 
-                  </div>
+   @foreach($subcategories as $subcategory)
+	    <ul>
+	      <li><a href="#">
+	      	@if(session()->get('language') == 'hindi') {{ $subcategory->subcategory_name_hin }} @else {{ $subcategory->subcategory_name_en }} @endif</a></li>
 
-                  @endforeach
+	    </ul>
+	@endforeach 
+
+
+	  </div>
+	  <!-- /.accordion-inner --> 
+	</div>
+	<!-- /.accordion-body --> 
+	</div>
+	<!-- /.accordion-group -->
+    @endforeach   
                   <!-- /.accordion-group -->
                   
                   
@@ -260,72 +264,76 @@ Tag Wish Product
 <div class="tab-pane active " id="grid-container">
 <div class="category-product">
   <div class="row">
+@foreach($products as $product)
+  <div class="col-sm-6 col-md-4 wow fadeInUp">
+    <div class="products">
+      <div class="product">
+        <div class="product-image">
+          <div class="image"> <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}">
+            <img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
+          <!-- /.image -->
 
-@foreach ($products as $product)
-  
+           @php
+        $amount = $product->selling_price - $product->discount_price;
+        $discount = ($amount/$product->selling_price) * 100;
+        @endphp     
 
-
-    <div class="col-sm-6 col-md-4 wow fadeInUp">
-      <div class="products">
-        <div class="product">
-          <div class="product-image">
-            <div class="image"> <a href="{{url('product/details/'.$product->id.'/'.$product->product_slug_en)}}">
-              <img  src=" {{asset($product->product_thumbnail)}}" alt=""></a> </div>
-            <!-- /.image -->
-            
-                @php
-                            $amount = $product->selling_price - $product->discount_price;
-                            $discount = ($amount/$product->selling_price) * 100;
-                          @endphp
-
-                          <div>
-                            @if ($product->discount_price == NULL)
-                                <div class="tag new"><span>new</span></div>
-                                @else
-                                  <div class="tag hot"><span>{{round($discount)}} %</span></div>
-                            @endif
-                          </div>
-           
-          <!-- /.product-image -->
-          
-          <div class="product-info text-left">
-          <h3 class="name"><a href="{{url('product/details/'.$product->id.'/'.$product->product_slug_en)}}">
-                            @if(session()->get('language') == 'hindi') {{$product->product_name_hin}} @else {{$product->product_name_en}} @endif
-                            </a></h3>
-            <div class="rating rateit-small"></div>
-            <div class="description"></div>
-             @if ($product->discount_price == NULL)
-                           <div class="product-price"> <span class="price"> ₹{{$product->selling_price}}</span> </div>
-
-                           @else
-                             <div class="product-price"> <span class="price"> ₹{{$product->discount_price}}</span> <span class="price-before-discount">₹{{$product->selling_price}}</span> </div>
-                           @endif
-            <!-- /.product-price --> 
-            
+          <div>
+            @if ($product->discount_price == NULL)
+            <div class="tag new"><span>new</span></div>
+            @else
+            <div class="tag hot"><span>{{ round($discount) }}%</span></div>
+            @endif
           </div>
-          <!-- /.product-info -->
-          <div class="cart clearfix animate-effect">
-            <div class="action">
-              <ul class="list-unstyled">
-                <li class="add-cart-button btn-group">
-                  <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
-                  <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-                </li>
-                <li class="lnk wishlist"> <a class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
-                <li class="lnk"> <a class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
-              </ul>
-            </div>
-            <!-- /.action --> 
-          </div>
-          <!-- /.cart --> 
+
+
         </div>
-              <!-- /.product --> 
-              
-            </div>
-            <!-- /.products --> 
-          </div>
+        <!-- /.product-image -->
 
-          @endforeach
+        <div class="product-info text-left">
+          <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}">
+          	@if(session()->get('language') == 'hindi') {{ $product->product_name_hin }} @else {{ $product->product_name_en }} @endif</a></h3>
+          <div class="rating rateit-small"></div>
+          <div class="description"></div>
+
+
+@if ($product->discount_price == NULL)
+<div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span>   </div>
+
+@else
+
+<div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">$ {{ $product->selling_price }}</span> </div>
+@endif
+
+
+
+
+          <!-- /.product-price --> 
+
+        </div>
+        <!-- /.product-info -->
+        <div class="cart clearfix animate-effect">
+          <div class="action">
+            <ul class="list-unstyled">
+              <li class="add-cart-button btn-group">
+                <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
+                <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+              </li>
+              <li class="lnk wishlist"> <a class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+              <li class="lnk"> <a class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
+            </ul>
+          </div>
+          <!-- /.action --> 
+        </div>
+        <!-- /.cart --> 
+      </div>
+      <!-- /.product --> 
+
+    </div>
+    <!-- /.products --> 
+  </div>
+  <!-- /.item -->
+  @endforeach
           <!-- /.item -->
           
           
@@ -362,9 +370,16 @@ Tag Wish Product
       <!-- /.col -->
       <div class="col col-sm-8 col-lg-8">
         <div class="product-info">
-          <h3 class="name"><a href="detail.html">Floral Print Buttoned</a></h3>
+         @if(session()->get('language') == 'hindi') {{ $product->product_name_hin }} @else {{ $product->product_name_en }} @endif</a></h3>
           <div class="rating rateit-small"></div>
-          <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$ 800</span> </div>
+          
+@if ($product->discount_price == NULL)
+<div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span>   </div>
+
+@else
+
+<div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">$ {{ $product->selling_price }}</span> </div>
+@endif
           <!-- /.product-price -->
           <div class="description m-t-10">Suspendisse posuere arcu diam, id accumsan eros pharetra ac. Nulla enim risus, facilisis bibendum gravida eget, lacinia id purus. Suspendisse posuere arcu diam, id accumsan eros pharetra ac. Nulla enim risus, facilisis bibendum gravida eget.</div>
           <div class="cart clearfix animate-effect">
