d8e60f779d1fec8e15cfccf5c6c01d16a437bf26
justcodingman <joginder9868@outlook.com>
2022-03-05 11:36:55 +0530

    modified:   resources/views/frontend/body/header.blade.php 	modified:   resources/views/frontend/master_home.blade.php

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -144,21 +144,16 @@
             </a>
             <ul class="dropdown-menu">
               <li>
-                <div class="cart-item product-summary">
-                  <div class="row">
-                    <div class="col-xs-4">
-                      <div class="image"> <a href="detail.html"><img src="{{asset('frontend/assets/images/cart.jpg')}}" alt=""></a> </div>
-                    </div>
-                    <div class="col-xs-7">
-                      <h3 class="name"><a href="index.php?page-detail">Simple Product</a></h3>
-                      <div class="price">$600.00</div>
-                    </div>
-                    <div class="col-xs-1 action"> <a href="#"><i class="fa fa-trash"></i></a> </div>
-                  </div>
-                </div>
-                <!-- /.cart-item -->
-                <div class="clearfix"></div>
-                <hr>
+               
+{{-- Mini cart START with AJax --}}
+
+<div id="miniCart">
+
+  
+</div>
+{{-- End Mini cart START with AJax --}}
+
+
                 <div class="clearfix cart-total">
                   <div class="pull-right"> <span class="text">Sub Total :</span><span class='price'>$600.00</span> </div>
                   <div class="clearfix"></div>
