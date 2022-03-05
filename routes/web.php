8491dc6d0f9df3b670dc486f90acba32ac887b3b
justcodingman <joginder9868@outlook.com>
2022-03-05 05:57:33 +0530

    new file:   app/Http/Controllers/Frontend/CartController.php 	modified:   composer.json 	modified:   composer.lock 	new file:   config/cart.php 	modified:   resources/views/frontend/master_home.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\LanguageController;
+use App\Http\Controllers\Frontend\CartControllerr;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\SliderController;
